87ecf23edf730f8bcfe1d8b9c0362ea38a461d09
cortadocodes <cortado.codes@protonmail.com>
2025-04-09 16:28:02 +0100

    DOC: Document inputs and outputs

--- variables.tf
+++ variables.tf
@@ -1,33 +1,39 @@
 variable "server_service_account_email" {
   type = string
+  description = "The email address of the service account for running the Django server."
 }
 
 
 variable "project" {
-  type    = string
-  default = "bezier-app"
+  type        = string
+  default     = "bezier-app"
+  description = "The ID of the GCP project to deploy resources in."
 }
 
 
 variable "resource_affix" {
-  type    = string
-  default = "bezier"
+  type        = string
+  default     = "bezier"
+  description = "The affix to add to each resource controlled by this module."
 }
 
 
 variable "environment" {
-  type    = string
-  default = "main"
+  type        = string
+  default     = "main"
+  description = "The name of the environment to deploy the resources in (must be one word with no hyphens or underscores in). This can be derived from a Terraform workspace name and used to facilitate e.g. testing and staging environments alongside the production environment ('main')."
 }
 
 
 variable "create_public_bucket" {
-  type    = bool
-  default = false
+  type        = bool
+  default     = false
+  description = "If `true`, create a public assets bucket."
 }
 
 
 variable "deletion_protection" {
-  type    = bool
-  default = false
+  type        = bool
+  default     = false
+  description = "If `true`, disallow deletion of the cloud storage buckets. `terraform apply` must be run after setting this to `false` before `terraform destroy` will work."
 }
